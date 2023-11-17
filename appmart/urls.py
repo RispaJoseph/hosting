070b9237da65d06034cdf8ebc4a2ead5f21d3f8f
RispaJoseph <rizpahjosph@gmail.com>
2023-11-17 17:55:57 +0530

    added product display

--- appmart/urls.py
+++ appmart/urls.py
@@ -4,4 +4,7 @@ from appmart import views
 app_name = "appmart"
 urlpatterns = [
     path('',views.index,name='index'),
+    path('category/',views.category_list_view,name='category'),
+    path('category/<cid>',views.category_product_list_view,name='category_product_list'),
+    path('product_all_list/',views.product_all_list, name='product_all_list'),
 ]
--- appmart/views.py
+++ appmart/views.py
@@ -15,3 +15,32 @@ def index(request):
         "category":category
     }
     return render(request, 'mart/index.html',context)
+
+def category_list_view(request):
+    categories = Category.objects.all()
+    context = {
+        "categories":categories
+    }
+    return render(request, 'mart/category_list.html',context)
+
+
+def category_product_list_view(request, cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(category=category)
+
+    context = {
+        "category":category,
+        "products":products,
+    }
+    return render(request, "mart/category_product_list.html",context)
+
+def product_all_list(request):
+    products = Product.objects.all()
+    category = Category.objects.all()
+
+    context = {
+        "products":products,
+        "category":category,
+    }
+    return render(request, 'mart/product_all_list.html',context)
+
